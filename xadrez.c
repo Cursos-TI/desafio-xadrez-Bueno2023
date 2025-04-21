5f8b68f3194a1586c63b6c49fb6540f7e50cfddf
Renan  Felipe Timóteo Bueno <125695342+Bueno2023@users.noreply.github.com>
2025-04-21 21:18:31 +0000

    Movimentos Cavalo

--- xadrez.c
+++ xadrez.c
@@ -4,10 +4,11 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    
+int main()
+{
 
-    for(int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         printf("Movimento da Torre: Direita\n"); // Movimento da Torre: cinco casas para a direita
     }
 
@@ -15,7 +16,8 @@ int main() {
 
     printf("Movimento do bispo: Diagonal\n"); // Movimento do bispo: diagonal
     int b = 1;
-    while (b <= 5) {
+    while (b <= 5)
+    {
         printf("Cima, Direita\n");
         b++;
     }
@@ -25,12 +27,28 @@ int main() {
     // Movimento da Rainha: oito casas para a esquerda (utilizando do-while)
     printf("Movimento da Rainha:\n");
     int k = 1;
-    do {
+    do
+    {
         printf("Esquerda\n");
         k++;
     } while (k <= 8);
 
-    
+    // Movimentos do Cavalo:
+    printf("\n"); // Linha em Branco (Pular linha)
+
+    printf("Movimento do cavalo: \n"); // Titulo movimento
+    for (int c = 1; c <= 2; c++) // Movendo duas casas para baixo (usando um loop for)
+    {
+        printf("Baixo\n");
+    }
+
+    // Movendo uma casa para a esquerda (usando um loop while)
+    int j = 0;
+    while (j < 1)
+    {
+        printf("Esquerda\n");
+        j++;
+    }
+
     return 0;
 }
-
